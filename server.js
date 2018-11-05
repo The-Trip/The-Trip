ee3ae631ebc56d08f5b15abe287b4adc8833e076
Tom <tomjbast@users.noreply.github.com>
2018-11-05 15:12:07 +0000

    Project Base - Page renders "Hello"

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use('/static', express.static('static'));
+app.set('view engine', 'hbs');
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.listen(8080, function(){
+    console.log('Listening on port 8080');
+});
