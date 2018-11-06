5d880d24e4f8fcd8d776900fd54809d19a1ac337
Lemony <lemonystudio@gmail.com>
2018-11-06 14:19:00 +0000

    Add trip_url to db sql

--- database.sql
+++ database.sql
@@ -6,8 +6,8 @@ INSERT INTO customer VALUES (4, 'Jon', 'jon@gmail.com');
 INSERT INTO customer VALUES (5, 'William', 'william@gmail.com');
 ALTER SEQUENCE customer_id_seq RESTART WITH 6 INCREMENT BY 1;
 
-INSERT INTO trip VALUES (1, 'Mark and Emma Go Away', 'London', 'New York',1);
-INSERT INTO trip VALUES (2, 'Jon''s Solo Adventure', 'Berlin', 'Rome',4);
+INSERT INTO trip VALUES (1, 'electric-dog', 'Mark and Emma Go Away', 'London', 'New York',1);
+INSERT INTO trip VALUES (2, 'messy-self', 'Jon''s Solo Adventure', 'Berlin', 'Rome',4);
 ALTER SEQUENCE trip_id_seq RESTART WITH 3 INCREMENT BY 1;
 
 INSERT INTO suggestion VALUES (1, 'Bobs Burgers', 'Best Burgers in Town', 1,2);
@@ -36,7 +36,8 @@ PRIMARY KEY (id)
 -- DYNAMIC TABLE FOR TRIP(S) (MANY-)
 CREATE TABLE trip (
 id serial,
-trip_name varchar(50) NOT NULL UNIQUE,
+trip_url VARCHAR(100) NOT NULL UNIQUE,
+trip_name VARCHAR(50) NOT NULL UNIQUE,
 origin VARCHAR(50) NOT NULL,
 destination VARCHAR(50) NOT NULL,
 trip_owner_id INT NOT NULL,
