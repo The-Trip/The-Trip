235f9e336803553444e600d477e9e7e5e3ecc3e8
Tom <tomjbast@users.noreply.github.com>
2018-11-06 12:19:28 +0000

    Added SQL database insertions

--- src/containers/FlightWrapper.js
+++ src/containers/FlightWrapper.js
@@ -8,6 +8,8 @@ const mapStateToProps = state => {
     }
 };
 
+//Currently only one way flights, need to do round trip
+
 // price is in .price
 //departure time is in .dTimeUTC
 //arrival time is in .aTimeUTC
@@ -15,6 +17,11 @@ const mapStateToProps = state => {
 // city from is .cityFrom
 //city to is .cityTo
 // bags price is an object in .bags_price. Numbered 1 and 2.
+
+//UTC timezones.
+//Need list of timezone deviance from UTC
+//Need number of seconds in an hour (3600)
+
 
 const mapDispatchToProps = dispatch => {
     return {
