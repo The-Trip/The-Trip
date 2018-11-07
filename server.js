eda37ce31cdeda85acba29d3d32aa7f68b811744
Chris Phillips <chrisphillers@gmail.com>
2018-11-07 14:48:11 +0000

    fetch npm install

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const fetch = require('node-fetch');
 const pgp = require('pg-promise')();
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
