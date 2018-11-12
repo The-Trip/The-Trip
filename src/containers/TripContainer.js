7155a4e4be22052f1f288d8fcef94cf007878fd6
Phil B <phil@berryman.org.uk>
2018-11-12 22:11:59 +0000

    fixed issues 33 and 45

--- src/containers/TripContainer.js
+++ src/containers/TripContainer.js
@@ -1,19 +1,26 @@
 import { connect } from "react-redux";
 import Trip from "../components/Trip";
+import { fetchTripsFromDB } from "../actions/phil";
 
 const mapStateToProps = (state, ownProps) => {
   const trips = state.trips;
-  const tripId = ownProps.match.params.id;
-  const trip = trips.find(trip => trip.id == tripId);
+  const tripId = parseInt(ownProps.match.params.id, 10);
+  const trip = trips.find(trip => trip.id === tripId);
+  const userId = state.user.id;
 
   return {
     tripId,
-    trip
+    trip,
+    userId
   };
 };
 
 const mapDispatchToProps = dispatch => {
-  return {};
+  return {
+    fetchTripsFromDB: userId => {
+      dispatch(fetchTripsFromDB(userId));
+    }
+  };
 };
 
 export default connect(
