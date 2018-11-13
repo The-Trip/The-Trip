71fe2da7dbf33d693aecb75bb1b0fbcf27367da9
Lemony <lemonystudio@gmail.com>
2018-11-13 16:48:38 +0000

    merging flights and comments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,31 +12,27 @@ import endDate from "./endDate";
 import suggestionForm from "./suggestionForm";
 import addedTripId from "./addedTripId";
 import setFlightsAPIResults from "./setFlightsAPIResults";
-import flightsToCity from "./flightsToCity";
-import flightsFromCity from "./flightsFromCity";
 import googlePlaceInfo from "./googlePlaceInfo";
-import isAPILoading from './isAPILoading'
+import isAPILoading from "./isAPILoading";
 import comments from "./comments";
 import stylesSwitches from "./stylesSwitches";
 
 export default combineReducers({
-    flightAPIResults: setFlightsAPIResults,
-    tripForm,
-    suggestionForm,
-    user,
-    googlePlaceInfo,
-    suggestions,
-    trips,
-    addedTripId,
-    view,
-    selectedPlace,
-    loginForm,
-    suggestionComment,
-    startDate,
-    endDate,
-    isAPILoading,
-    cityFrom: flightsFromCity,
-    cityTo: flightsToCity,
-    comments,
-    stylesSwitches
+  flightAPIResults: setFlightsAPIResults,
+  tripForm,
+  suggestionForm,
+  user,
+  googlePlaceInfo,
+  suggestions,
+  trips,
+  addedTripId,
+  view,
+  selectedPlace,
+  loginForm,
+  suggestionComment,
+  startDate,
+  endDate,
+  isAPILoading,
+  comments,
+  stylesSwitches
 });
