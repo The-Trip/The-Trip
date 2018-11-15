e53bbe213957f2b760cbe294e068870cb3996df2
Tom <tomjbast@users.noreply.github.com>
2018-11-15 11:03:06 +0000

    Small update. One container test written

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -2,7 +2,7 @@ import { connect } from "react-redux";
 import Home from "../components/Home.js";
 import { setView } from "../actions/phil";
 
-const mapStateToProps = state => {
+export const mapStateToProps = state => {
   return {
     view: state.view
   };
