8a748723f2adaee1883f48f57c5f5357fad20af5
Lemony <lemonystudio@gmail.com>
2018-11-17 12:41:10 +0000

    added tabs and routes

--- src/components/TripsList.js
+++ src/components/TripsList.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Route, NavLink, Switch } from "react-router-dom";
 import TripsListItem from "./TripsListItem.js";
 import "../styles/components/TripsList.scss";
-// import TripCreationContainer from "../containers/TripCreationContainer.js";
+import "../styles/base/tabs.scss";
 
 class TripsList extends React.Component {
   componentDidMount() {
@@ -9,6 +10,9 @@ class TripsList extends React.Component {
   }
 
   render() {
+    const myTripsUrl = `/trips/`;
+    const friendsTripsUrl = `/trips/friends`;
+
     return (
       <div>
         <React.Fragment>
@@ -19,9 +23,57 @@ class TripsList extends React.Component {
               <h1 className="trips__title">Your trips</h1>
             </header> */}
 
-            {this.props.trips.map(trip => {
+            <div className="tabs tabs-style-topline">
+              <nav className="tabs__nav">
+                <ul className="tabs__navlist">
+                  <li className="tabs__navitem">
+                    <NavLink
+                      to={myTripsUrl}
+                      className="mytrips-toggle icon icon-user"
+                    >
+                      <span>My trips</span>
+                    </NavLink>
+                  </li>
+                  <li className="tabs__navitem">
+                    <NavLink
+                      to={friendsTripsUrl}
+                      className="friendstrips-toggle icon icon-userfriends"
+                    >
+                      <span>Friends&rsquo; trips</span>
+                    </NavLink>
+                  </li>
+                </ul>
+              </nav>
+
+              <div className="tabs__content">
+                <Switch>
+                  <Route
+                    path="/trips/"
+                    render={() => (
+                      <section className="myView">
+                        {this.props.trips.map(trip => {
+                          return <TripsListItem key={trip.id} trip={trip} />;
+                        })}
+                      </section>
+                    )}
+                  />
+                  <Route
+                    path="/trips/friends"
+                    render={() => (
+                      <section className="friendsView">
+                        {this.props.trips.map(trip => {
+                          return <TripsListItem key={trip.id} trip={trip} />;
+                        })}
+                      </section>
+                    )}
+                  />
+                </Switch>
+              </div>
+            </div>
+
+            {/* {this.props.trips.map(trip => {
               return <TripsListItem key={trip.id} trip={trip} />;
-            })}
+            })} */}
           </section>
         </React.Fragment>
       </div>
