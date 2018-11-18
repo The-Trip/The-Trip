4bf0f44f8df7018066277a7ae593736c4104fff0
Phil B <phil@berryman.org.uk>
2018-11-18 16:05:34 +0000

    create trip working for unregistered users

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.get("/api/airports", function(req, res) {
   res.json(results);
 });
 
-app.post("/api/trip", (req, res) => {
+app.post("/api/trip", isLoggedIn, (req, res) => {
   const randomNum = Math.floor(Math.random() * tripWordsArray.length);
   const randomArrayValue = tripWordsArray[randomNum];
   const destinationSplit = req.body.trip.destination.split(" ");
@@ -326,6 +326,8 @@ app.get("/api/checklogin/", function(req, res) {
 
 app.get("/api/user/trip", isLoggedIn, function(req, res) {
   const userId = req.user.id;
+  console.log("trip get");
+  console.log(userId);
   // console.log(req.params)
   db.any(
     "SELECT trip.id, trip.url, trip.name, trip.origin, trip.destination, trip.details, trip.image, trip.customer_id, trip.time, permission.permission, permission.customer_id FROM trip, permission WHERE permission.customer_id = $1 AND trip.id = permission.trip_id ORDER BY trip.time DESC",
