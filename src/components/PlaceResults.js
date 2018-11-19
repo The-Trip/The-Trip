ac3ce4fd2edec2e176905cf27755eee77aac7417
Phil B <phil@berryman.org.uk>
2018-11-19 15:47:26 +0000

    changed onMouseDown to onClick in PlaceResults

--- src/components/PlaceResults.js
+++ src/components/PlaceResults.js
@@ -13,7 +13,7 @@ class PlaceResults extends React.Component {
           {this.props.googlePlaceInfo.slice(0, 5).map(place => (
             <li
               key={place.place_id}
-              onMouseDown={() => this.props.selectPlace(place.place_id)}
+              onClick={() => this.props.selectPlace(place.place_id)}
             >
               {place.name}
               {/* {place.types[0] === 'night_club' ? 'nightclub' : place.types[0]} */}
