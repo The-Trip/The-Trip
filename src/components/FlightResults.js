53f872fd67415d0655354abaa95c468012643edf
Lemony <lemonystudio@gmail.com>
2018-12-04 15:14:02 +0000

    styles for selected flights

--- src/components/FlightResults.js
+++ src/components/FlightResults.js
@@ -1,71 +1,72 @@
 import React from "react";
 import "../styles/components/FlightResults.scss";
 
-function FlightResults({ flightResults, isAPILoading, startDate, endDate, tripId, fetchFlightsFromDB }) {
-
+function FlightResults({
+  flightResults,
+  isAPILoading,
+  startDate,
+  endDate,
+  tripId,
+  fetchFlightsFromDB
+}) {
   function addZero(i) {
-        if (i < 10) {
-            i = "0" + i;
-        }
-        return i;
+    if (i < 10) {
+      i = "0" + i;
     }
+    return i;
+  }
 
-    function handleClick(
-        flightDetail,
-        departureTimeReturn,
-        arrivalTimeReturn,
-        arrivalTimeOutbound,
-        departureTimeOutbound,
-        startDate,
-        endDate,
-        tripId
-    ) {
+  function handleClick(
+    flightDetail,
+    departureTimeReturn,
+    arrivalTimeReturn,
+    arrivalTimeOutbound,
+    departureTimeOutbound,
+    startDate,
+    endDate,
+    tripId
+  ) {
+    let flightDetailsObject = {
+      tripId: tripId,
+      flightCombinationID: flightDetail.route[0].combination_id,
+      outboundFlightDate: startDate.toISOString(),
+      returnFlightDate: endDate.toISOString(),
+      cityFrom: flightDetail.route[0].cityFrom,
+      cityTo: flightDetail.route[0].cityTo,
+      airportFrom: flightDetail.route[0].flyFrom,
+      airportTo: flightDetail.route[0].flyTo,
+      outboundLocalArrivalTime: `${addZero(
+        arrivalTimeOutbound.getHours()
+      )}:${addZero(arrivalTimeOutbound.getMinutes())}:00`,
+      outboundLocalDepartureTime: `${addZero(
+        departureTimeOutbound.getHours()
+      )}:${addZero(departureTimeOutbound.getMinutes())}:00`,
+      returnLocalDepartureTime: `${addZero(
+        departureTimeReturn.getHours()
+      )}:${addZero(departureTimeReturn.getMinutes())}:00`,
+      returnLocalArrivalTime: `${addZero(
+        arrivalTimeReturn.getHours()
+      )}:${addZero(arrivalTimeReturn.getMinutes())}:00`,
+      price: flightDetail.price
+    };
 
-        let flightDetailsObject = {
-            tripId: tripId,
-            flightCombinationID: flightDetail.route[0].combination_id,
-            outboundFlightDate: startDate.toISOString(),
-            returnFlightDate: endDate.toISOString(),
-            cityFrom: flightDetail.route[0].cityFrom,
-            cityTo: flightDetail.route[0].cityTo,
-            airportFrom: flightDetail.route[0].flyFrom,
-            airportTo: flightDetail.route[0].flyTo,
-            outboundLocalArrivalTime: `${addZero(
-                arrivalTimeOutbound.getHours()
-            )}:${addZero(arrivalTimeOutbound.getMinutes())}:00`,
-            outboundLocalDepartureTime: `${addZero(
-                departureTimeOutbound.getHours()
-            )}:${addZero(departureTimeOutbound.getMinutes())}:00`,
-            returnLocalDepartureTime: `${addZero(
-                departureTimeReturn.getHours()
-            )}:${addZero(departureTimeReturn.getMinutes())}:00`,
-            returnLocalArrivalTime: `${addZero(arrivalTimeReturn.getHours())}:${addZero(
-                arrivalTimeReturn.getMinutes()
-            )}:00`,
-            price: flightDetail.price
-        };
+    addFlightToDB(flightDetailsObject).then(() => {
+      fetchFlightsFromDB(tripId);
+    });
+  }
 
-
-        addFlightToDB(flightDetailsObject)
-            .then(() => {
-                fetchFlightsFromDB(tripId)
-            })
-
-    }
-
-    function addFlightToDB(flightObject) {
-        console.log("add flight to DB running");
-        return fetch("/api/flights", {
-            method: "post",
-            body: JSON.stringify({ flightObject }),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-            .then(response => response.json())
-            .then(data => console.log("Response data", data));
-    }
-
+  function addFlightToDB(flightObject) {
+    console.log("add flight to DB running");
+    return fetch("/api/flights", {
+      method: "post",
+      body: JSON.stringify({ flightObject }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(data => console.log("Response data", data));
+  }
 
   if (isAPILoading) {
     return (
@@ -92,6 +93,7 @@ function FlightResults({ flightResults, isAPILoading, startDate, endDate, tripId
       </header>
 
       {flightResults.map(function(flightDetail) {
+        let id = flightDetail.route[0].combination_id;
         let arrivalTimeUTCOutbound = flightDetail.route[0].aTime;
         let departureTimeUTCOutbound = flightDetail.route[0].dTime;
         let arrivalTimeOutbound = new Date(arrivalTimeUTCOutbound * 1000);
@@ -103,7 +105,7 @@ function FlightResults({ flightResults, isAPILoading, startDate, endDate, tripId
         let departureTimeReturn = new Date(departureTimeUTCReturn * 1000);
 
         return (
-          <article className="results">
+          <article key={id} className="results">
             <div className="results__display container">
               <table className="results__table">
                 <tbody>
@@ -124,7 +126,7 @@ function FlightResults({ flightResults, isAPILoading, startDate, endDate, tripId
 
                   <tr className="results__details details--out">
                     <td>{flightDetail.route[0].flyFrom}</td>
-                    <td>Emirates</td>
+                    <td>&nbsp;</td>
                     <td>{flightDetail.route[0].flyTo}</td>
                   </tr>
 
@@ -146,7 +148,7 @@ function FlightResults({ flightResults, isAPILoading, startDate, endDate, tripId
 
                   <tr className="results__details details--return">
                     <td>{flightDetail.route[1].flyFrom}</td>
-                    <td>Emirates</td>
+                    <td>&nbsp;</td>
                     <td>{flightDetail.route[1].flyTo}</td>
                   </tr>
                 </tbody>
@@ -166,7 +168,7 @@ function FlightResults({ flightResults, isAPILoading, startDate, endDate, tripId
                       departureTimeOutbound,
                       startDate,
                       endDate,
-                      tripId,
+                      tripId
                     )
                   }
                 >
@@ -174,58 +176,6 @@ function FlightResults({ flightResults, isAPILoading, startDate, endDate, tripId
                 </button>
               </div>
             </div>
-
-            {/* <p>
-              Outbound: {flightDetail.route[0].cityFrom} -{" "}
-              {flightDetail.route[0].flyFrom} to {flightDetail.route[0].cityTo}{" "}
-              - {flightDetail.route[0].flyTo}
-            </p>
-            <p>
-              Departure Time (local time):{" "}
-              {`${addZero(departureTimeOutbound.getHours())}:${addZero(
-                departureTimeOutbound.getMinutes()
-              )}:00`}
-            </p>
-            <p>
-              Arrival Time (local time):{" "}
-              {`${addZero(arrivalTimeOutbound.getHours())}:${addZero(
-                arrivalTimeOutbound.getMinutes()
-              )}:00`}
-            </p>
-            <p>
-              Return: {flightDetail.route[1].cityFrom} -{" "}
-              {flightDetail.route[1].flyFrom} to {flightDetail.route[1].cityTo}{" "}
-              - {flightDetail.route[1].flyTo}
-            </p>
-            <p>
-              Departure Time (local time):{" "}
-              {`${addZero(departureTimeReturn.getHours())}:${addZero(
-                departureTimeReturn.getMinutes()
-              )}:00`}
-            </p>
-            <p>
-              Arrival Time (local time):{" "}
-              {`${addZero(arrivalTimeReturn.getHours())}:${addZero(
-                arrivalTimeReturn.getMinutes()
-              )}:00`}
-            </p>
-            <h2>Total Price: £{flightDetail.price}</h2>
-            <button
-              type="button"
-              onClick={() =>
-                handleClick(
-                  flightDetail,
-                  departureTimeReturn,
-                  arrivalTimeReturn,
-                  arrivalTimeOutbound,
-                  departureTimeOutbound,
-                  startDate,
-                  endDate
-                )
-              }
-            >
-              Pick this Flight!
-            </button> */}
           </article>
         );
       })}
--- src/components/SelectedTripFlights.js
+++ src/components/SelectedTripFlights.js
@@ -1,69 +1,118 @@
 import React from "react";
-import {loginUser} from "../actions/phil";
-import {receiveFlightsFromDB} from "../actions/tomactions";
+import "../styles/components/SelectedTripFlights.scss";
+import { loginUser } from "../actions/phil";
+import { receiveFlightsFromDB } from "../actions/tomactions";
 
-class SelectedTripFlights extends React.Component{
-    constructor(){
-        super();
+class SelectedTripFlights extends React.Component {
+  constructor() {
+    super();
 
-        this.handleClick = this.handleClick.bind(this);
-        this.removeFlightFromDB = this.removeFlightFromDB.bind(this)
+    this.handleClick = this.handleClick.bind(this);
+    this.removeFlightFromDB = this.removeFlightFromDB.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.fetchFlightsFromDB(this.props.tripId);
+  }
+
+  // componentDidUpdate(){
+  //     this.props.fetchFlightsFromDB(this.props.tripId)
+  // }
+
+  removeFlightFromDB(flightId) {
+    console.log("start of removing flight");
+    return fetch(`/api/flights/${flightId}`, {
+      method: "delete"
+    })
+      .then(response => response.json())
+      .then(flightIdRemoved => {
+        console.log(flightIdRemoved);
+      });
+  }
+
+  handleClick(flightId) {
+    console.log("You clicked remove flight");
+    this.removeFlightFromDB(flightId).then(() => {
+      let updatedFlightsFromDB = this.props.flightsFromDB.filter(
+        flight => flight.id !== flightId
+      );
+      this.props.receiveFlightsFromDB(updatedFlightsFromDB);
+    });
+  }
+
+  render() {
+    if (!this.props.flightsFromDB) {
+      return null;
+    } else {
+      return (
+        <div>
+          {this.props.flightsFromDB.map(flight => {
+            return (
+              <article key={flight.id} className="selected">
+                <div className="selected__display">
+                  <table className="selected__table">
+                    <tbody>
+                      <tr className="selected__city">
+                        <td>{flight.city_from}</td>
+                        <td>
+                          <td>
+                            <p>&pound;{flight.price}</p>
+                          </td>
+                        </td>
+                        <td>{flight.city_to}</td>
+                      </tr>
+                      <tr className="selected__date">
+                        <td>{flight.outbound_flight_date}</td>
+                        <td>
+                          <td>&nbsp;</td>
+                        </td>
+                        <td>{flight.return_flight_date}</td>
+                      </tr>
+                      <tr className="selected__outbound">
+                        <td>{flight.outbound_local_departure_time}</td>
+                        <td>
+                          <i className="fas fa-plane" />
+                        </td>
+                        <td>{flight.outbound_local_arrival_time}</td>
+                      </tr>
+
+                      <tr className="selected__details details--out">
+                        <td>{flight.airport_from}</td>
+                        <td>&nbsp;</td>
+                        <td>{flight.airport_to}</td>
+                      </tr>
+
+                      <tr className="selected__return">
+                        <td>{flight.return_local_departure_time}</td>
+                        <td>
+                          <i className="fas fa-plane" />
+                        </td>
+                        <td>{flight.return_local_arrival_time}</td>
+                      </tr>
+
+                      <tr className="selected__details details--return">
+                        <td>{flight.airport_to}</td>
+                        <td>&nbsp;</td>
+                        <td>{flight.airport_from}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+
+                  <p>Price: {flight.price}</p>
+                  <button
+                    type="button"
+                    onClick={() => this.handleClick(flight.id)}
+                  >
+                    {" "}
+                    Remove
+                  </button>
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      );
     }
-
-    componentDidMount(){
-        this.props.fetchFlightsFromDB(this.props.tripId)
-    }
-
-    // componentDidUpdate(){
-    //     this.props.fetchFlightsFromDB(this.props.tripId)
-    // }
-
-    removeFlightFromDB(flightId) {
-        console.log("start of removing flight");
-            return fetch(`/api/flights/${flightId}`, {
-                method: "delete"
-            })
-                .then(response => response.json())
-                .then(flightIdRemoved => {
-                   console.log(flightIdRemoved)
-                });
-    }
-
-    handleClick(flightId){
-        console.log("You clicked remove flight");
-        this.removeFlightFromDB(flightId)
-            .then(() =>{
-                let updatedFlightsFromDB = this.props.flightsFromDB.filter(flight => flight.id !== flightId);
-                this.props.receiveFlightsFromDB(updatedFlightsFromDB)
-            })
-    }
-
-    render() {
-        if (!this.props.flightsFromDB){
-            return null
-        } else {
-         return (
-            <div>
-                {
-                    this.props.flightsFromDB.map(flight => {
-                        return (
-                            <div>
-                                <p>Origin Airport: {flight.airport_from} - {flight.city_from}</p>
-                                <p>Destination Airport: {flight.airport_to} - {flight.city_to}</p>
-                                <p>Outbound Flight Date: {flight.outbound_flight_date}</p>
-                                <p>Outbound Flight Departure Time: {flight.outbound_local_departure_time}</p>
-                                <p>Outbound Flight Arrival Time: {flight.outbound_local_arrival_time}</p>
-                                <p>Return Flight Date: {flight.return_flight_date}</p>
-                                <p>Return Flight Departure Time: {flight.return_local_departure_time}</p>
-                                <p>Return Flight Arrival Time: {flight.return_local_arrival_time}</p>
-                                <p>Price: {flight.price}</p>
-                                <button type="button" onClick={() => this.handleClick(flight.id)}> Remove</button>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-         )}
-    }
+  }
 }
-export default SelectedTripFlights
+export default SelectedTripFlights;
